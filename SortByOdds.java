206ca23b6cbb56181f146f80d12912e616cb14cb
Kotikov <11deadsluts@gmail.com>
2023-02-22 01:37:58 +0300

    SortByOdds is ready

---
+++ SortByOdds.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class SortByOdds {
+    // Пусть дан произвольный список целых чисел, удалить из него четные числа
+    public static void main(String[] args) {
+        System.out.println("input a value that will be the length of your list: ");
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        input.close();
+        ArrayList<Integer> yourList = new ArrayList<>();
+        yourList = listCreator(length);
+        System.out.printf("it's your created list of numbers %s \n", yourList);
+        evenRemover(yourList);
+    }
+
+    public static ArrayList<Integer> listCreator(int length) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        Random rnd = new Random();
+        for (int i = 0; i < length; i++) {
+
+            list.add(rnd.nextInt(8888));
+        }
+        return list;
+    }
+
+    public static void evenRemover(ArrayList<Integer> newList) {
+        int stopNum = newList.size();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < stopNum; i++) {
+            if (newList.get(i) % 2 != 0) {
+
+                list.add(newList.get(i));
+
+            }
+        }
+        System.out.printf("it's your new list without evens %s", list);
+    }
+}
