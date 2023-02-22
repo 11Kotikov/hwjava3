b60e8e7720be2a7e9936b00c6d13579aa254405a
Kotikov <11deadsluts@gmail.com>
2023-02-22 23:52:00 +0300

    it's over, i think

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+// 1. Пусть дан произвольный список целых чисел, удалить из него четные числа
+// 2) Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("input a value that will be the length of your list: ");
         Scanner input = new Scanner(System.in);
@@ -12,6 +15,7 @@ public class Main {
         ArrayList<Integer> yourList = new ArrayList<>();
         yourList = listCreator(length);
         System.out.printf("it's your created list of numbers %s \n", yourList);
+        minMaxMid(yourList);
         evenRemover(yourList);
     }
 
@@ -36,6 +40,30 @@ public class Main {
 
             }
         }
-        System.out.printf("it's your new list without evens %s", list);
+        System.out.printf("it's your new list without evens %s \n", list);
     }
+
+    public static void minMaxMid(ArrayList<Integer> mmmList) {
+        int sum = 0;
+        int max = mmmList.get(0);
+        int min = mmmList.get(0);
+        for (int i = 0; i < mmmList.size(); i++) {
+            sum = sum + mmmList.get(i);
+        }
+        System.out.printf("Your average value is %s\n", sum / mmmList.size());
+
+        for (int i = 0; i < mmmList.size(); i++) {
+            if (mmmList.get(i) > max) {
+                max = mmmList.get(i);
+            }
+        }
+        System.out.printf("the maxxx %s \n", max);
+        for (int i = 0; i < mmmList.size(); i++) {
+            if (mmmList.get(i) < min) {
+                min = mmmList.get(i);
+            }
+        }
+        System.out.printf("the minnn %s \n", min);
+    }
+
 }
